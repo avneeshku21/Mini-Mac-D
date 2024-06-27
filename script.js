365556c144bc35fd7f1559d54a7a0bde568157ec
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-27 12:24:29 +0530

    update

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const hamburgerIcon=document.querySelector('.hamburger-menu-container')
-const headerContent=document.querySelector('.header-content')
-const closeIcon=document.querySelector('.close-icon')
-const nav=document.querySelector('nav')
+const hamburgerIcon = document.querySelector('.hamburger-menu-container')
+const headerContent = document.querySelector('.header-content')
+const closeIcon = document.querySelector('.close-icon')
+const nav = document.querySelector('nav')
 
-
-hamburgerIcon.addEventListener('click',(e)=>{
+hamburgerIcon.addEventListener('click', (e) => {
     e.stopPropagation()
-headerContent.classList.add('.menu-open')
+    headerContent.classList.add('menu-open')
 })
 
-nav.addEventListener('click',(e)=>{
-e.stopPropagation()
+nav.addEventListener('click', (e) => {
+    e.stopPropagation()
 })
 
-closeIcon.addEventListener('click',()=>{
+closeIcon.addEventListener('click', () => {
     headerContent.classList.remove('menu-open')
 })
-window.addEventListener('click',()=>{
+
+window.addEventListener('click', () => {
     headerContent.classList.remove('menu-open')
 })
